e366220de7f225fc1e67d9ee0eee430ee3a84019
Alessandro Valenza <alessandro.valenza@edu-its.it>
2026-04-02 11:02:48 +0200

    ho finito il cifrario

---
+++ Caesar_alghorythm/caesar_cipher.py
@@ -0,0 +1,46 @@
+
+def caesar(message, key) -> str:
+    result = ""
+    for char in message:
+#65-90
+        if 'A' <= char <= 'Z':
+            ascii_offset = 65
+            new_char = chr((ord(char) - ascii_offset + key) % 26 + ascii_offset)
+            result += new_char
+
+#97-122
+        elif 'a' <= char <= 'z':
+            ascii_offset = 97
+            new_char = chr((ord(char) - ascii_offset + key) % 26 + ascii_offset)
+            result += new_char
+        else:
+            result += char
+
+    return result
+
+
+def main():
+    print(f"CIFRARIO DI CESARE")
+    choice = int(input("[1]Cifrare, [2]Decifrare\n"))
+
+    if choice not in [1, 2]:
+        print("invalid choice")
+        return
+
+    message = input("Inserisci il contenuto da cifrare/decifrare\n").strip().upper()
+    try:
+        key = int(input("Inserisci chiave\n"))
+    except ValueError:
+            print("Errore: La chiave deve essere un numero intero.\n")
+            return
+
+    if choice == 1:
+        result = caesar(message, key)
+        print(f"\nTesto Cifrato: {result}")
+    elif choice == 2:
+        result = caesar(message, -key) #passo il negativo della chiave
+        print(f"\nTesto Decifrato: {result}")
+
+if __name__ == "__main__":
+    main()
+
